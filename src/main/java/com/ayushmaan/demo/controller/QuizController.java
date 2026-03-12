a171fe35f5a5bf8e212d2e1733653fe7ba46246f
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2026-03-13 04:12:57 +0530

    getquizzes

--- src/main/java/com/ayushmaan/demo/controller/QuizController.java
+++ src/main/java/com/ayushmaan/demo/controller/QuizController.java
@@ -1,17 +1,30 @@
 package com.ayushmaan.demo.controller;
 
 
-import jakarta.persistence.SqlResultSetMapping;
+import com.ayushmaan.demo.models.Question;
+import com.ayushmaan.demo.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("question")
 public class QuizController {
 
+    @Autowired
+    QuestionService questionService;
+
     @GetMapping("allQuestions")
-    public String getAllQuestions(){
-        return "Hi From Server";
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("debug")
+    public String debugQuestions() {
+        List<Question> list = questionService.getAllQuestions();
+        return "Number of questions found: " + list.size();
     }
 }
---
+++ src/main/java/com/ayushmaan/demo/service/QuestionService.java
@@ -0,0 +1,19 @@
+package com.ayushmaan.demo.service;
+
+import com.ayushmaan.demo.dao.QuestionDao;
+import com.ayushmaan.demo.models.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
+}
